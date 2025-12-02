b104a8576bd363dca3c668754e36c7c6d550eb07
Gael Vazquez <gxv220028@utdallas.org>
2025-12-02 14:32:17 -0600

    loading in every individual file of the gif to make a library

--- src/project.py
+++ src/project.py
@@ -5,13 +5,18 @@ def main():
 	pygame.init()
 	pygame.display.set_caption("Where's Waldo?")
 
+	# load all 149 frames into a list
+	frames = []
+	for frame in range(150):
+		img = pygame.image.load(f'frame_{frame:03d}_delay-0.04s.gif').convert_alpha()
+		frames.append(img)
+		pygame.image.load(f'frame_{frame:03d}_delay-0.04s.gif')
 	dt = 0
 	resolution = (961, 964)
 	# create the display first, then load/scale the background image
 	screen = pygame.display.set_mode(resolution)
 	bg_image = pygame.image.load('waldo.png')
-	explosion_1 = pygame.image.load('waldo_explosion_1.gif')
-	explosion_2 = pygame.image.load('waldo_explosion_secret.gif')
+	explosion_1 = pygame.image.load('')
 	bg_image = pygame.transform.scale(bg_image, resolution)
 	start_time = pygame.time.get_ticks()
 	show_image = False
